ac8a61d333b927f1b48726a1378c54099e6ae4c0
Virangya <guitarlass@gmail.com>
2024-09-07 13:32:27 +0530

    comment 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 # Create the main window
 root = tk.Tk()
@@ -15,6 +16,23 @@ label.pack(pady=10)
 text_area = tk.Text(root, wrap="word", font=("Arial", 24), height=15, width=28)
 text_area.pack(pady=20)
 
+type_timer = None
+
+def hide_all():
+    text_area.delete("1.0", "end")
+    messagebox.showerror("You waited too long!", "Your text is gone! Start all over.")
+
+
+def on_key_release(event):
+    global type_timer
+
+    if type_timer:
+        root.after_cancel(type_timer)
+
+    type_timer = root.after(2000, hide_all)
+
+
+text_area.bind("<KeyRelease>", on_key_release)
+
 # Run the Tkinter event loop
 root.mainloop()
-
